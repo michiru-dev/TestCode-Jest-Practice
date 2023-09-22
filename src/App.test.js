3cea3084a1b55f1f5584a4566931b461e7b18869
michiru6828 <since_l999@yahoo.co.jp>
2023-09-22 15:40:33 -0700

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  //render(どのコンポーネントをテストするか)
+  render(<App />)
+  //<App/>の中にlearn reactと書かれているかiは大文字小文字を無視する
+  const linkElement = screen.getByText(/learn react/i)
+  expect(linkElement).toBeInTheDocument()
+})
